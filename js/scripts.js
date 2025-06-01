70b8e30fa5ce0b2793e64eaebc144016a8e9df4e
kristiana19 <marusickristijana@gmail.com>
2025-06-01 17:07:36 +0200

    download cv

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,3 @@ window.addEventListener('load', () => {
     slideEl.style.animation = 'slideIn 1s ease-out forwards';
   }
 });
-
-document.getElementById("contactForm").addEventListener("submit", function(event){
-  event.preventDefault();
-
-  emailjs.sendForm('kristijana_ID', 'template_ID', this)
-    .then(() => {
-      alert("Message sent successfully!");
-    }, (error) => {
-      alert("Failed to send message: " + error.text);
-    });
-});
